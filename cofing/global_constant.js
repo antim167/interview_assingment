5d18ca82650064c19f1526a506ab539bfef5d7b0
antim167 <81341738+antim167@users.noreply.github.com>
2023-07-17 03:10:01 +0530

    Initial commit

---
+++ cofing/global_constant.js
@@ -0,0 +1,6 @@
+STAR_END_DATE_FORMAT = "YYYY-MM-DD";
+MONTH_DATE_FORMATE   = "MM/DD HH:mm:ss";
+DATE_MATCH_REGEX     = /\((\d{2}\/\d{2}\s\d{2}:\d{2}:\d{2})/
+LINE_MATCH_REGEX     = /\[(\w+)\]\s+ComputerName:([^ ]+)\s+UserID:([^ ]+)/;
+
+
---
+++ index.js
@@ -0,0 +1,62 @@
+const express    = require('express');
+const bodyParser = require('body-parser');
+const moment     = require('moment');
+const fs         = require('fs');
+const path       = require('path');
+const app        = express();
+
+app.use(bodyParser.json());
+require("./cofing/global_constant");
+
+/**  including .env file */
+require('dotenv').config();
+
+const logsDirectory = './public/logs';
+const disconnects = [];
+fs.readdirSync(logsDirectory).forEach((file) => {
+    const filePath = path.join(logsDirectory, file);
+    const logContents = fs.readFileSync(filePath, 'utf-8');
+    const logLines = logContents.split('\n');
+
+    logLines.forEach((line) => {
+        const match = LINE_MATCH_REGEX.exec(line);
+        if (match) {
+            const [, , computerName, userId] = match;
+            const dateMatch = DATE_MATCH_REGEX.exec(line);
+            const date = dateMatch ? moment(dateMatch[1], 'MM/DD HH:mm:ss').toDate() : null;
+            disconnects.push({ computerName, userId, date });
+        }
+    });
+});
+
+/** Get data with the specified date range */
+app.post('/api/computer_disconnects', (req, res) => {
+    const { startDate, endDate } = req.body;
+
+   /** Convert date strings to Date objects */
+    const start = moment(startDate, STAR_END_DATE_FORMAT).toDate();
+    const end   = moment(endDate, STAR_END_DATE_FORMAT).toDate();
+
+    /**  Filter specified date range */
+    const filteredDisconnects = disconnects.filter((record) => {
+        return record.date >= start && record.date <= end;
+    });
+
+    /** Group and count the sdisconnects by computer name */
+    const computerDisconnectCounts = filteredDisconnects.reduce((acc, record) => {
+        acc[record.computerName] = (acc[record.computerName] || 0) + 1;
+        return acc;
+    }, {});
+
+    /**Sort the result in descending order */
+    const sortedDisconnects = Object.entries(computerDisconnectCounts).sort(
+        (a, b) => b[1] - a[1]
+    );
+
+    res.send(sortedDisconnects);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
